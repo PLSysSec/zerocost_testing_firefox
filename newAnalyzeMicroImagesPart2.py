2773c9bd06cf0664f969e3c5c246716b9b5cb133
shravanrn@gmail.com <shravanrn@gmail.com>
2020-11-23 17:55:04 -0800

    Update benchmark scripts for mpk build

--- newAnalyzeMicroImagesPart2.py
+++ newAnalyzeMicroImagesPart2.py
@@ -13,18 +13,38 @@ def getMedian(els, group):
             return float(el["Median"].replace(',', ''))
     raise RuntimeError("Group not found")
 
-def computeSummary(summaryFile, ext, parsed1, parsed2, parsed3):
+gResLabel = {
+    "1920" : "\\n1280p",
+    "480" : "{0}\\n320p",
+    "240" : "\\n135p",
+    "width_10": "10",
+    "width_20": "20",
+    "width_30": "30",
+    "width_60": "60",
+    "width_120": "120",
+    "width_240": "240",
+    "width_480": "480",
+    "width_960": "960",
+    "width_1440": "1440",
+    "width_1920": "1920",
+}
+
+def computeSummary(summaryFile, ext, parsed1, parsed2, parsed3, parsed4):
     with open(summaryFile, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(["Image", "FullSave", "RegSave"])
+        writer.writerow(["Image", "FullSave", "RegSave", "MPKFullSave"])
         for qual in ["best", "default", "none"]:
-            for res, label in {"1920" : "\\n1280p", "480" : "{0}\\n320p", "240" : "\\n135p", "10" : "\\n10w"}.items():
+            for res, label in gResLabel.items():
                 try:
                     group_suffix = qual + "_" + res + "." + ext
                     zerocost_val = getMedian(parsed1, group_suffix)
                     fullsave_val = getMedian(parsed2, group_suffix)
                     regsave_val = getMedian(parsed3, group_suffix)
-                    writer.writerow([ label.replace("{0}", qual), fullsave_val/zerocost_val, regsave_val/zerocost_val ])
+                    mpkfullsave_val = getMedian(parsed4, group_suffix)
+                    writer.writerow([ label.replace("{0}", qual),
+                        fullsave_val/zerocost_val,
+                        regsave_val/zerocost_val,
+                        mpkfullsave_val/zerocost_val ])
                 except:
                     pass
 
@@ -43,10 +63,12 @@ def main():
     input1 = read(inputFolderName, "zerocost_terminal_analysis.json")
     input2 = read(inputFolderName, "fullsave_terminal_analysis.json")
     input3 = read(inputFolderName, "regsave_terminal_analysis.json")
+    input4 = read(inputFolderName, "mpkfullsave_terminal_analysis.json")
 
     parsed1 = json.loads(input1)["data"]
     parsed2 = json.loads(input2)["data"]
     parsed3 = json.loads(input3)["data"]
+    parsed4 = json.loads(input4)["data"]
 
-    computeSummary(os.path.join(inputFolderName, "jpeg_perf.dat"), "jpeg", parsed1, parsed2, parsed3)
+    computeSummary(os.path.join(inputFolderName, "jpeg_perf.dat"), "jpeg", parsed1, parsed2, parsed3, parsed4)
 main()
