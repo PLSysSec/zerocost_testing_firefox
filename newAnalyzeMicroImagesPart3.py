706ed8bcae663568dfbb6165ab46c028e3d7e636
shravanrn@gmail.com <shravanrn@gmail.com>
2021-01-19 17:35:07 -0800

    Fix bug in script

--- newAnalyzeMicroImagesPart3.py
+++ newAnalyzeMicroImagesPart3.py
@@ -73,7 +73,7 @@ def main():
         exit(1)
     inputFolderName = sys.argv[1]
 
-    if os.path.exists(os.path.join(inputFolderName, "zerocost_terminal_analysis.json"))):
+    if os.path.exists(os.path.join(inputFolderName, "zerocost_terminal_analysis.json")):
         parsed1 = json.loads(read(inputFolderName, "zerocost_terminal_analysis.json"))["data"]
         parsed2 = json.loads(read(inputFolderName, "fullsave_terminal_analysis.json"))["data"]
         parsed3 = json.loads(read(inputFolderName, "regsave_terminal_analysis.json"))["data"]
@@ -81,12 +81,12 @@ def main():
         parsed5 = json.loads(read(inputFolderName, "fullsavewindows_terminal_analysis.json"))["data"]
         computeSummary64(os.path.join(inputFolderName, "all_compare.dat"), parsed1, parsed2, parsed3, parsed4, parsed5)
 
-    if os.path.exists(os.path.join(inputFolderName, "mpkfullsave_terminal_analysis.json"))):
+    if os.path.exists(os.path.join(inputFolderName, "mpkfullsave_terminal_analysis.json")):
         parsed1 = json.loads(read(inputFolderName, "stockindirect_terminal_analysis.json"))["data"]
         parsed2 = json.loads(read(inputFolderName, "mpkfullsave_terminal_analysis.json"))["data"]
         computeSummaryTwo(os.path.join(inputFolderName, "all_comparempk.dat"), parsed1, parsed2, "MPK")
 
-    if os.path.exists(os.path.join(inputFolderName, "segmentsfi_terminal_analysis.json"))):
+    if os.path.exists(os.path.join(inputFolderName, "segmentsfi_terminal_analysis.json")):
         parsed1 = json.loads(read(inputFolderName, "stockindirect32_terminal_analysis.json"))["data"]
         parsed2 = json.loads(read(inputFolderName, "segmentsfi_terminal_analysis.json"))["data"]
         computeSummaryTwo(os.path.join(inputFolderName, "all_compare32.dat"), parsed1, parsed2, "SegmentSfi")
